292731f5fc05188fd053d8943e0a610bd3075da7
Dema Lhamo <12190047.gcit@rub.edu.bt>
2021-04-29 12:36:44 +0600

    Lab10

---
+++ Lab/Lab10/app/src/main/java/edu/gcit/lab9/Calculator.java
@@ -0,0 +1,20 @@
+package edu.gcit.lab9;
+
+public class Calculator {
+
+    public double Add(double Value1, double Value2){
+        return Value1 + Value2;
+    }
+
+    public double Sub(double Value1, double Value2){
+        return Value1 - Value2;
+    }
+
+    public double Mul(double Value1, double Value2){
+        return Value1 * Value2;
+    }
+
+    public double Div(double Value1, double Value2){
+        return Value1 / Value2;
+    }
+}
