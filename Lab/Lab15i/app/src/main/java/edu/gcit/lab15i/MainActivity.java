423654d9ebe7cc720236be458ea871642c52132d
Dema Lhamo <12190047.gcit@rub.edu.bt>
2021-05-13 18:53:51 +0600

    todo17

--- Lab/Lab15i/app/src/main/java/edu/gcit/lab15i/MainActivity.java
+++ Lab/Lab15i/app/src/main/java/edu/gcit/lab15i/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
+
                 viewPager.setCurrentItem(tab.getPosition());
             }
 
